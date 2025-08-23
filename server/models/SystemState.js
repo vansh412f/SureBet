93f1b2233964dcfd02c5d7efe765b68cbec9bc09
Vansh <singhrishi814@gmail.com>
2025-08-24 03:02:15 +0530

    Update SystemState.js

--- server/models/SystemState.js
+++ server/models/SystemState.js
@@ -1,13 +1,11 @@
-// /models/SystemState.js
 const mongoose = require('mongoose');
 
 const SystemStateSchema = new mongoose.Schema(
   {
-    key: { type: String, unique: true, required: true }, // e.g., "api_state"
-    value: { type: Number, required: true }, // e.g., currentKeyIndex
+    key: { type: String, unique: true, required: true }, 
+    value: { type: Number, required: true },
   },
   { timestamps: true }
 );
 
-// Simple key-value store for system-wide state
 module.exports = mongoose.model('SystemState', SystemStateSchema);
